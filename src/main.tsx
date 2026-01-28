f118771fec9d702f0d4a15c3dfd7f59989eeadfb
Philipe Firmo de Jesus <philipe.jesus@zello.tec.br>
2026-01-27 23:55:25 -0300

    Cleanup: remove debug logs

--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,13 @@ import App from './App'
 import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 
-console.log('Iniciando aplicação...');
-
-try {
-  const rootElement = document.getElementById('root');
-  if (!rootElement) {
-    console.error('Elemento root não encontrado!');
-  } else {
-    console.log('Elemento root encontrado, montando React...');
-    createRoot(rootElement).render(
-      <StrictMode>
-        <ErrorBoundary>
-          <App />
-        </ErrorBoundary>
-      </StrictMode>,
-    )
-    console.log('React montado (ou processo iniciado).');
-  }
-} catch (error) {
-  console.error('Erro fatal na inicialização:', error);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </StrictMode>,
+  )
 }
